85a5eb3c07c127a0d7f28488070ea8fe463dd654
Corey Carter <crc2231@gmail.com>
2020-10-01 21:17:19 -0600

    Refactor

---
+++ formatter.rb
@@ -0,0 +1,56 @@
+require_relative 'near_earth_objects'
+class Formatter
+
+    def initialize
+    end
+
+    def welcome
+        puts "________________________________________________________________________________________________________________________________"
+        puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+        puts "Please enter a date in the following format YYYY-MM-DD."
+        print ">>"
+    end
+
+    def assign_data(asteroid_details)
+        @asteroid_list = asteroid_details[:asteroid_list]
+        @total_number_of_asteroids = asteroid_details[:total_number_of_asteroids]
+        @largest_asteroid = asteroid_details[:biggest_asteroid]
+    end
+
+    def create_columns
+        column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
+        @column_data = column_labels.each_with_object({}) do |(col, label), hash|
+            hash[col] = {
+            label: label,
+            width: [@asteroid_list.map { |asteroid| asteroid[col].size }.max, label.size].max}
+        end
+        @header = "| #{ @column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
+        @divider = "+-#{@column_data.map { |_,col| "-"*col[:width] }.join('-+-') }-+"
+    end
+
+    def format_row_data(row_data, column_info)
+        row = row_data.keys.map { |key| row_data[key].ljust(column_info[key][:width]) }.join(' | ')
+        puts "| #{row} |"
+    end
+
+    def create_rows(asteroid_data, column_info)
+        rows = asteroid_data.each { |asteroid| format_row_data(asteroid, column_info) }
+    end
+
+    def formated_date
+        DateTime.parse(@@date).strftime("%A %b %d, %Y")
+    end
+
+    def presentation
+        puts "______________________________________________________________________________"
+        puts "On #{formated_date}, there were #{@total_number_of_asteroids} objects that almost collided with the earth."
+        puts "The largest of these was #{@largest_asteroid} ft. in diameter."
+        puts "\nHere is a list of objects with details:"
+        puts @divider
+        puts @header
+        create_rows(@asteroid_list, @column_data)
+        puts @divider
+    end
+
+
+end
--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -5,7 +5,8 @@ require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
   def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.find_neos_by_date('2019-03-30')
-    assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+    results = NearEarthObjects.new('2019-03-30')
+    asteroids = results.find_neos_by_date
+    assert_equal '(2019 GD4)', asteroids[:asteroid_list][0][:name]
   end
 end
